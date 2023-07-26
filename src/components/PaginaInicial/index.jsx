c8606c654dc588c3b5c89c1918995da0523ce8d1
Kauan pc giovanna <Kauang13@gmail.com>
2023-07-26 16:41:53 -0300

    menu e cabecalho concluido

---
+++ src/components/PaginaInicial/index.jsx
@@ -0,0 +1,12 @@
+import Cabecalho from "../Cabecalho";
+import Menu from "../Menu";
+
+
+export default function PaginaInicial() {
+    return (
+        <>
+            <Cabecalho/>
+            <Menu/>
+        </>
+    )
+}
---
+++ src/pages/PaginaInicial/index.jsx
@@ -0,0 +1,12 @@
+import Cabecalho from "../Cabecalho";
+import Menu from "../menu";
+
+
+export default function PaginaInicial() {
+    return (
+        <>
+            <Cabecalho/>
+            <Menu/>
+        </>
+    )
+}
